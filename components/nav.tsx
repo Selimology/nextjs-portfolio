3521416d735f888adb2e30ab48b727b584311b07
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-05-30 22:53:53 +0200

    Added Darkmode and fixed issue.

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,28 +7,13 @@ const Nav = () => {
     const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
 
-    const ToggleThemeIcon = () => {
-        const currentTheme = theme === 'light' ? true : 'dark';
-        if (currentTheme === 'dark') {
-            return (
-                <button
-                    aria-label="Toggle Theme"
-                    className="flex justify-center items-center  ">
-                    <IoSunny className="text-white" />
-                </button>
-            );
-        } else {
-            return <IoMoon className="" onClick={() => setTheme('dark')} />;
-        }
-    };
-
     //avoid hydration mismatch
     useEffect(() => {
         setMounted(true);
-    });
+    }, []);
     return (
         <div className="flex justify-center flex-col px-24">
-            <nav className="flex items-center justify-between w-full flex-1 relative max-w-2xl  mx-auto py-3">
+            <nav className="flex items-center justify-between w-full flex-1 relative max-w-2xl  mx-auto py-3 ">
                 <Link href="/">
                     <a>
                         <h2>Selim</h2>
@@ -36,31 +21,39 @@ const Nav = () => {
                 </Link>
 
                 <div className="flex space-x-10 items-center  ">
-                    <Link href="/">
+                    <Link href="/about">
                         <a>
                             <h2>About</h2>
                         </a>
                     </Link>
-                    <Link href="/">
+                    <Link href="/projects">
                         <a>
                             <h2>Projects</h2>
                         </a>
                     </Link>
-                    <Link href="/">
+                    <Link href="/blog">
                         <a>
                             <h2>Blog</h2>
                         </a>
                     </Link>
-                    <Link href="/">
+                    <Link href="/contact">
                         <a>
                             <h2>Contact</h2>
                         </a>
                     </Link>
                     <button
                         aria-label="Toggle Theme"
-                        className="w-8 h-8 text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900">
-                        <IoMoon onClick={() => setTheme('light')} />
-                        <IoSunny onClick={() => setTheme('dark')} />
+                        className="w-8 h-8  flex justify-center items-center
+                         rounded-lg focus:outline-none focus:shadow-outline   hover:bg-gray-200 hover:ring-gray-300 hover:ring-2
+                          dark:hover:ring-gray-700 dark:hover:bg-gray-700 "
+                        onClick={() =>
+                            setTheme(theme === 'dark' ? 'light' : 'dark')
+                        }>
+                        {mounted && theme === 'dark' ? (
+                            <IoSunny className="text-white" />
+                        ) : (
+                            <IoMoon className="text-black" />
+                        )}
                     </button>
                 </div>
             </nav>
