67d43ae1c2dd7e0ad9309fa9ed2be3d6fe853e5c
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-05-31 19:28:54 +0200

    Added Mobile Responsive, still WP

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { IoMoon, IoSunny } from 'react-icons/io5';
 import { useTheme } from 'next-themes';
 import { useState, useEffect } from 'react';
+import MobileMenu from './mobileMenu';
 
 const Nav = () => {
     const [mounted, setMounted] = useState(false);
@@ -11,50 +12,59 @@ const Nav = () => {
     useEffect(() => {
         setMounted(true);
     }, []);
-    return (
-        <div className="flex justify-center flex-col px-24">
-            <nav className="flex items-center justify-between w-full flex-1 relative max-w-2xl  mx-auto py-3 ">
-                <Link href="/">
-                    <a>
-                        <h2>Selim</h2>
-                    </a>
-                </Link>
 
-                <div className="flex space-x-10 items-center  ">
-                    <Link href="/about">
+    return (
+        <div className=" w-full py-6 flex justify-center">
+            <nav className=" flex items-center w-full justify-evenly md:max-w-2xl md:justify-between min-w-min">
+                <div className="visible md:hidden">
+                    <MobileMenu />
+                </div>
+                <div className="flex md:flex-1">
+                    <Link href="/">
                         <a>
-                            <h2>About</h2>
+                            <h2>Selim</h2>
                         </a>
                     </Link>
-                    <Link href="/projects">
-                        <a>
-                            <h2>Projects</h2>
-                        </a>
-                    </Link>
-                    <Link href="/blog">
-                        <a>
-                            <h2>Blog</h2>
-                        </a>
-                    </Link>
-                    <Link href="/contact">
-                        <a>
-                            <h2>Contact</h2>
-                        </a>
-                    </Link>
-                    <button
-                        aria-label="Toggle Theme"
-                        className="w-8 h-8  flex justify-center items-center
+                </div>
+                <div className="flex ">
+                    <div className=" hidden md:flex  items-center">
+                        <Link href="/about">
+                            <a className="ml-10">
+                                <h2>About</h2>
+                            </a>
+                        </Link>
+                        <Link href="/projects">
+                            <a className="ml-10">
+                                <h2>Projects</h2>
+                            </a>
+                        </Link>
+                        <Link href="/blog">
+                            <a className="ml-10">
+                                <h2>Blog</h2>
+                            </a>
+                        </Link>
+                        <Link href="/contact">
+                            <a className="ml-10">
+                                <h2>Contact</h2>
+                            </a>
+                        </Link>
+                    </div>
+                    <div>
+                        <button
+                            aria-label="Toggle Theme"
+                            className="md:ml-10 w-8 h-8  flex justify-center items-center
                          rounded-lg focus:outline-none focus:shadow-outline   hover:bg-gray-200 hover:ring-gray-300 hover:ring-2
                           dark:hover:ring-gray-700 dark:hover:bg-gray-700 "
-                        onClick={() =>
-                            setTheme(theme === 'dark' ? 'light' : 'dark')
-                        }>
-                        {mounted && theme === 'dark' ? (
-                            <IoSunny className="text-white" />
-                        ) : (
-                            <IoMoon className="text-black" />
-                        )}
-                    </button>
+                            onClick={() =>
+                                setTheme(theme === 'dark' ? 'light' : 'dark')
+                            }>
+                            {mounted && theme === 'dark' ? (
+                                <IoSunny className="text-white" />
+                            ) : (
+                                <IoMoon className="text-black" />
+                            )}
+                        </button>
+                    </div>
                 </div>
             </nav>
         </div>
